4d50f8126fccf3e5a69b833691fbf48ac580638a
Avinash <avinashpsk@gmail.com>
2020-06-27 23:50:40 +0530

    fix: Binary Search

--- Divide_And_Conquer/8binarySearch.js
+++ Divide_And_Conquer/8binarySearch.js
@@ -5,6 +5,7 @@ function search(arr, val) {
     let max = arr.length - 1;
     while (min <= max) {
         let middle = Math.floor((min + max) / 2);
+        let currentElement = arr[middle];
         if (arr[middle] < val) {
             min = middle + 1;
         }
@@ -12,14 +13,16 @@ function search(arr, val) {
             max = middle - 1;
         }
         else {
-            return middle;
+            if (currentElement === val) {
+                return middle;
+            }
         }
     }
-    return -1;
+    return 'not present';
 }
 
 let t1 = performance.now();
-const result = search([1, 5, 8, 11, 24, 56, 67, 76, 89, 94, 110]);
+const result = search([1, 5, 8, 11, 24, 56, 67, 76, 89, 94, 110], 76);
 console.log(result);
 let t2 = performance.now();
 console.log(`Time Lapse: ${(t2 - t1) / 1000} seconds`);
